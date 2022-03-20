f2ef9f55a342d7fdab8818c3937f76db29ff6c07
Alejandro Caravaca Puchades <acaravacapuchades@icloud.com>
2022-03-20 15:33:35 +0100

    Implement patient_ids project

--- bin/projects/patient_ids.py
+++ bin/projects/patient_ids.py
@@ -6,14 +6,11 @@ from projects  import Project, project
 from serialize import load_data
 
 
-@project(name='patient_ids')
+@project('patient_ids')
 class PatientIDs(Project):
 
 	def __init__(self, datadir: Path):
 		self._patients = load_data(datadir, 'ufmn/patients')
-
-	def describe(self) -> None:
-		pass
 
 	def export_data(self) -> DataFrame:
 		patients = self._patients[['nhc', 'dni', 'cip']]
