272bf100cea4ac7ee1a62b1685ed2090a5aad79e
Alejandro Caravaca Puchades <acaravacapuchades@icloud.com>
2022-05-23 00:27:45 +0200

    Add missing fields in setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,19 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
+from pathlib import Path
 
-setup(name='HUB-Datatools',
-      version='1.0.0',
+root_dir = Path(__file__).parent
+long_description = root_dir.joinpath('README.md').read_text()
+
+setup(name='hub-datatools',
+      version='1.0.2',
       author='Alejandro Caravaca Puchades',
       author_email='acaravacapuchades@icloud.com',
       url='https://github.com/acpuchades/hub-datatools',
       description='A set of utilities to help with data analysis tasks',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
       packages=[
           'hub_datatools',
       ],
@@ -18,4 +24,21 @@ setup(name='HUB-Datatools',
               'dt-search=hub_datatools.scripts.search:main',
           ],
       },
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Environment :: Console',
+          'Intended Audience :: Science/Research',
+          'Intended Audience :: Healthcare Industry',
+          'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+          'Natural Language :: English',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 3.10',
+          'Programming Language :: Python :: 3.11',
+          'Programming Language :: Python :: 3.12',
+          'Topic :: Scientific/Engineering',
+          'Topic :: Scientific/Engineering :: Bio-Informatics',
+          'Topic :: Scientific/Engineering :: Information Analysis',
+          'Topic :: Scientific/Engineering :: Medical Science Apps.',
+      ]
       )
