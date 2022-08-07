bbf0304c2e0090e6efe124a906629f5843eb0d84
Alejandro Caravaca Puchades <acaravacapuchades@icloud.com>
2022-08-07 15:35:03 +0200

    1.4.0

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ root_dir = Path(__file__).parent
 long_description = root_dir.joinpath('README.md').read_text()
 
 setup(name='hub-datatools',
-      version='1.3.1',
+      version='1.4.0',
       author='Alejandro Caravaca Puchades',
       author_email='acaravacapuchades@icloud.com',
       url='https://github.com/acpuchades/hub-datatools',
